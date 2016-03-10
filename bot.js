b0dd61c1997663a00fc175f944274d4fd4de9785
Gary Schulte <gary.schulte@jivesoftware.com>
2016-03-09 17:19:10 -0800

    fuck

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ function respond() {
     botId = process.env.BOT_ID;
   }
 
-  if (true) {
+  if (request.group_id=="20116625") {
     //(request.name.substring(0,6) != "Undead" && botRegex.test(request.text)) || request.group_id='20116625'
     // && botRegex.test(request.text)) {
     this.res.writeHead(200);
